6dd11528e43a969adb23253241c54e2b8078437e
jatink124 <kaushaljatin48@gmail.com>
2024-08-28 07:14:04 +0530

    homechange

---
+++ src/Components/ChecklistCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ChecklistCard = () => {
+  return (
+    <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-8">
+      <div className="p-6">
+        <h2 className="text-2xl font-bold mb-4 text-gray-800">Trading Checklist</h2>
+        <ul className="list-disc list-inside text-gray-700">
+          <li className="mb-2">Control your trades: take only 6 trades.</li>
+          <li className="mb-2">Apply Stop Limit Strategy when 5-minute candle is broken.</li>
+          <li className="mb-2">Use 10 EMA, 21 EMA, PDH, PDL support method with a high target and a stop loss of 155.</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ChecklistCard;
--- src/Components/Mhome.js
+++ src/Components/Mhome.js
@@ -1,13 +1,9 @@
 import React from 'react'
+import ChecklistCard from './ChecklistCard'
 
 function Mhome() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-    <p className="font-serif text-2xl text-gray-800">
-      Force your mind to think
-    </p>
-    <p>Strategy: Wait for Movement , Wait You have full day,Take 1 trade and wait for next movement,you get 300 to 500 wait for best next setup.</p>
-  </div>
+  <><ChecklistCard/></>
   )
 }
 
