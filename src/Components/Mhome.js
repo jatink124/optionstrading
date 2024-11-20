8cc2e42ef660bffcd7826be65d94e277bb5dbe51
jatink124 <kaushaljatin48@gmail.com>
2024-11-20 22:49:14 +0530

    select component

--- src/Components/Mhome.js
+++ src/Components/Mhome.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ChecklistCard from './ChecklistCard';
 import ParentComponent from './ParentComponent';
 import ThingsToAchieveCard from './ThingsToAchieveCard';
@@ -8,41 +8,71 @@ import PredictionCard from './Home/PredictionCard';
 import Showotstrategy from './Showotstrategy';
 
 function Mhome() {
+  const [selectedComponent, setSelectedComponent] = useState('');
+
+  // Handle dropdown selection
+  const handleDropdownChange = (e) => {
+    setSelectedComponent(e.target.value);
+  };
+
   return (
-    <div className="flex flex-wrap justify-center gap-4 p-4">
-      {/* ChecklistCard */}
-      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-        <ChecklistCard />
-      </div>
- {/* OptionTradingStrategy */}
- <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-       <Showotstrategy/>
-      </div>
-      {/* ParentComponent */}
-      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-        <ParentComponent />
-      </div>
-
-  {/* ParentComponent */}
-  <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-  <PredictionCard/>
-      </div>
-
-      {/* ThingsToAchieveCard */}
-      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-        <ThingsToAchieveCard />
+    <div className="p-4">
+      {/* Dropdown for selecting the component */}
+      <div className="mb-4">
+        <label className="block mb-2 font-semibold">Select Component</label>
+        <select
+          className="p-2 border rounded"
+          onChange={handleDropdownChange}
+          value={selectedComponent}
+        >
+          <option value="">Select</option>
+          <option value="ChecklistCard">ChecklistCard</option>
+          <option value="Showotstrategy">Showotstrategy</option>
+          <option value="ParentComponent">ParentComponent</option>
+          <option value="PredictionCard">PredictionCard</option>
+          <option value="ThingsToAchieveCard">ThingsToAchieveCard</option>
+          <option value="VKlevels">VKlevels</option>
+        </select>
       </div>
 
-      {/* ReadStrategy */}
-      {/* <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-        <ReadStrategy />
-      </div> */}
+      {/* Conditionally render components based on dropdown selection */}
+      <div className="flex flex-wrap justify-center gap-4">
+        {selectedComponent === 'ChecklistCard' && (
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+            <ChecklistCard />
+          </div>
+        )}
 
-      {/* VKlevels */}
-      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-        <VKlevels />
+        {selectedComponent === 'Showotstrategy' && (
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+            <Showotstrategy />
+          </div>
+        )}
+
+        {selectedComponent === 'ParentComponent' && (
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+            <ParentComponent />
+          </div>
+        )}
+
+        {selectedComponent === 'PredictionCard' && (
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+            <PredictionCard />
+          </div>
+        )}
+
+        {selectedComponent === 'ThingsToAchieveCard' && (
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+            <ThingsToAchieveCard />
+          </div>
+        )}
+
+        {selectedComponent === 'VKlevels' && (
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+            <VKlevels />
+          </div>
+        )}
       </div>
-      
     </div>
   );
 }
